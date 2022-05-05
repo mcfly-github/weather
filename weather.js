513c249d9ca1a96dcfb2996196d3c3c4263bb920
mcfly-github <92212396+mcfly-github@users.noreply.github.com>
2022-05-05 14:04:43 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,24 +1,23 @@
 
-
-
 export default function getWeatherData(lat, lon) {
     
-    const url = "https://api.openweathermap.org/data/2.5/weather?";
-    const units = "metric";
-    const appid = "4e026d1d0369716d521ce6646a92a040";
+    const url = "https://api.openweathermap.org/data/2.5/weather?"
+    const units = "metric"
+    const appid = "4e026d1d0369716d521ce6646a92a040"
     const dataParams = {"lat":lat,
                     "lon":lon,
                     "appid":appid,
                     "units":units
     }
-
+    
     fetch(url + new URLSearchParams(dataParams))
         .then(response => response.json())
-        //.then(data=>console.log(data));
         .then(data => {
-            let temp = data
-            document.getElementById("temperature").innerHTML = (temp['main']['temp'] + ' C°');
-        });
-    
-
+        document.getElementById("temperature")
+        .innerHTML = (data['main']['temp'] + ' C°');
+        document.getElementById("clouds")
+        .innerHTML = (data['weather'][0]['description']);
+        document.getElementById("wind-speed")
+        .innerHTML = (data['wind']['speed']);
+        })
 }
