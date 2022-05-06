3097ee1fd9d847b01e9111c432b942e096a823e7
mcfly-github <92212396+mcfly-github@users.noreply.github.com>
2022-05-06 16:08:00 +0200

    Update search.js

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ import getWeatherData from "./weather.js";
 
 export function searchCity() {
 
-    let choosenCity = document.getElementById("city").value;
+    let choosenCity = document.getElementById("search").value;
     const geoUrl = "http://api.openweathermap.org/geo/1.0/direct?";
     const appid = "4e026d1d0369716d521ce6646a92a040";
     const dataParams = {
