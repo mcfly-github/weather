5108b31153451eded588f833791ad8db684404e9
mcfly-github <martineleven@gmail.com>
2022-05-08 13:20:45 +0200

    update to https

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ import getWeatherData from "./weather.js";
 export function searchCity() {
 
     let choosenCity = document.getElementById("search").value
-    const geoUrl = "http://api.openweathermap.org/geo/1.0/direct?"
+    const geoUrl = "https://api.openweathermap.org/geo/1.0/direct?"
     const appid = "4e026d1d0369716d521ce6646a92a040"
     const dataParams = {
 
